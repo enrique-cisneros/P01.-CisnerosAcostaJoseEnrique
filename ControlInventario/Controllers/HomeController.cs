89282868be3d3895f20576f7443ffd17dbb3461b
enrique-cisneros <64668037+enrique-cisneros@users.noreply.github.com>
2020-05-11 20:04:28 -0700

    Layout VS Null

--- ControlInventario/Controllers/HomeController.cs
+++ ControlInventario/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace ControlInventario.Controllers
     {
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Principal");
+            }
             return View();
         }
 
